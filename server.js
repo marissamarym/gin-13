92352eb6ec7b47a594fb1060c618e82371cd7dd2
Glitch (hello-express) <none>
2018-09-30 15:40:54 +0000

    📼🚃 Checkpoint ./server.js:1853958/366 ./box2d.js:1853958/429968

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-var b2= require("./box2d")();
+var Box2D= require("./box2d");
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -22,13 +22,8 @@ var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+var world = new Box2D.Dynamics.b2World(
+	new b2Vec2(0, 10) //gravity
+	, true //allow sleep
+	);
 
-console.log(b2.b2Vec2);
-
-var world = new b2.b2World( new b2.b2Vec2(0.0, -10.0) );
-
-var groundBody = world.CreateBody( new b2.b2BodyDef() );
-
-var bodyDef = new b2.b2BodyDef();
-bodyDef.set_type( b2.b2_dynamicBody );
-var dynamicBody = world.CreateBody( bodyDef );
